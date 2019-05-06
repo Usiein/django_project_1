c40273a7108a5f42cab7b55ccc61092fc5440d8b
Usiein <usein.osmanov.ct@gmail.com>
2019-05-06 18:25:35 +0300

    several tests added to posts app

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Post
+from django.urls import reverse
 # Create your tests here.
 class PostModelTest(TestCase):
     def setUp(self):
@@ -10,3 +11,19 @@ class PostModelTest(TestCase):
         post = Post.objects.get(id=2)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'another test issue')
+
+class PostsPageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/posts/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('posts'))
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('posts'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'posts.html')
